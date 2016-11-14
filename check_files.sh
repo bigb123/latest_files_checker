b21556991eab07f481520834be6788a07e24ea78
bigb123 <piotr.palka123@gmail.com>
2016-11-14 01:15:06 +0100

    Find command finds all files and checking how old are they

--- check_files.sh
+++ check_files.sh
@@ -10,11 +10,23 @@
 PATH_TO_ANALYZE=$1 # probably /usr/local/music
 SECONDS_IN_MONTH=2360591 # amount of seconds in star month: 27 days 7 hours 43 minutes and 11 seconds
 
-# loop over all files
+function create_symlink {
+  echo "Create"
+}
 
-# if the file is older than a month the symlink will be created
 
-# check how old the file is: 
-#  difference between current amount of seconds since epoch and 
-echo $(( $(date +%s) - $(stat --printf "%Y" "$file") ))
+# find deals with decision stuff: 
+# - if file is newer than month create symlink if not exist
+# - if not - delete symlink if exist
+
+find "/usr/local/music/deep house" -type f ! -path "/usr/local/music/latest/*" -exec bash -c '
+  echo "{}"
+  # check how old the file is: 
+  #  difference between current amount of seconds since epoch and file amount of second since epoch since last data modification 
+  if [ $(( $(date +%s) - $(stat --printf "%Y" "{}") )) -lt $0 ]; then
+    $1
+  else 
+    echo "old"
+  fi
+' $SECONDS_IN_MONTH create_symlink \;
 
