da588402d6863a8e28d485987b50a07844508f08
bigb123 <piotr.palka123@gmail.com>
2016-11-14 18:14:41 +0100

    Creating and removing symlinks is working; need to improve removing symlinks philosophy

--- check_files.sh
+++ check_files.sh
@@ -3,9 +3,10 @@
 # Script to check latest files in folder and creating symlinks to them in another folder
 #
 #
-# Parameters:
-# 1 - path to folder to analyze
-# except folders
+# ToDO:
+# - parameters:
+#  - except folders
+#  - folders to analyze
 
 PATH_TO_ANALYZE=$1 # probably /usr/local/music
 PATH_WITH_SYMLINKS="/usr/local/music/latest/"
@@ -14,6 +15,31 @@ SECONDS_IN_MONTH=2360591 # amount of seconds in star month: 27 days 7 hours 43 m
 # check how old the file is: 
 # - if file is newer than given value create symlink if not exist
 # - if not - delete symlink if exist
+
+function create_symlink {
+  filepath="$1"
+  # checking if symlink already exist
+  # if not exist -create
+  local filename=$(basename "$filepath")
+  if [ ! -h "$PATH_WITH_SYMLINKS/$filename" ]; then
+    ln -s "$1" "$PATH_WITH_SYMLINKS"
+    echo "symlink created"
+  fi
+}
+
+# need to do better performance - no need to check every time if symlink exists
+# better go through folder with symlinks and remove old
+function remove_symlink {
+  filepath="$1"
+  # checking if symlink already exist
+  # if not exist -create
+  local filename=$(basename "$filepath")
+  # need to check if symlink exist; file name required; use "basename"
+  if [ -h "$PATH_WITH_SYMLINKS/$filename" ]; then
+    unlink "$PATH_WITH_SYMLINKS/$filename"
+    echo "symlink removed"
+  fi
+}
 
 function check_file_age {
   #  difference between current amount of seconds since epoch and file amount of second since epoch since last data modification 
@@ -24,17 +50,6 @@ function check_file_age {
   fi
 }
 
-function create_symlink {
-  echo "Create symlink"
-  # need to check if symlink already exist; file name required; use "basename"
-  ln -s "$1" "$PATH_WITH_SYMLINKS"
-}
-
-function remove_symlink {
-  echo "Remove symlink"
-  # need to check if symlink exist; file name required; use "basename"
-  unlink "$PATH_WITH_SYMLINKS"
-}
 
 # finds reular files in given path and write it to array
 find "/usr/local/music/deep house" -type f -print0 ! -path "/usr/local/music/latest/*" | while IFS= read -r -d $'\0' FileName ; do
